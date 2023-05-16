f0df097d8f01aa4fa7ca503b48e9c9977b0531cd
Malvin222 <127707299+Malvin222@users.noreply.github.com>
2023-05-16 12:58:06 +0900

    Delete study12 directory

--- study12/InvalidNmException.java
+++
@@ -1,9 +0,0 @@
-package study12;
-
-//사용자 exception 클래스 생성
-public class InvalidNmException	extends Exception {
-	public String toString() {
-		return "올바른 성이 아닙니다.";
-	}
-
-}
--- study12/MyException.java
+++
@@ -1,7 +0,0 @@
-package study12;
-
-public class MyException extends Exception{
-	public String toTest() {
-		return "에러입니다.";
-	}
-}//ExTest1로 넘어감
