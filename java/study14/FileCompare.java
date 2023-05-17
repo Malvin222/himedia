986fc92560729de4526f5f15fe113fcfab191b9d
Malvin222 <127707299+Malvin222@users.noreply.github.com>
2023-05-17 14:00:15 +0900

    Update FileCompare.java

--- java/study14/FileCompare.java
+++ java/study14/FileCompare.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class FileCompare {
-		//파일비교
+		//파일 내용 비교	
 	public static void main(String[] args) {
-		String file1 = "C:\\html_test\\html\\1st\\member_info1.txt";
-		String file2 = "C:\\html_test\\html\\1st\\member_info2.txt";
+		String file1 = "C:\\html_test\\html\\1st\\member_info1.txt";	//경로에 파일이 있어야함
+		String file2 = "C:\\html_test\\html\\1st\\member_info2.txt";	//경로에 파일이 있어야함
 		try {
 			FileReader fr1 =new FileReader(file1);
 			BufferedReader br1 =new BufferedReader(fr1);
@@ -15,19 +15,19 @@ public class FileCompare {
 			FileReader fr2 = new FileReader(file2);
 			BufferedReader br2 =new BufferedReader(fr2);
 			
-			String a ="";
+			String a ="";	//읽어온 내용을 임시로 담는 변수
 			String b ="";
-			int cnt= 0;
+			int cnt= 0;	//라인 수
 			while(true) {
-				if(((a= br1.readLine())!=null)&&((b=br2.readLine())!=null)) {
+				if(((a= br1.readLine())!=null)&&((b=br2.readLine())!=null)) {	//a값과 b값을 읽어옴
 				cnt++;
-				if(!a.equals(b));{
-				System.out.println(file1 + ":=> " +cnt+"line :"+a);
-				System.out.println(file2 + ":=> " +cnt+"line :"+b);
-				System.out.println();
-				}
-				}else break;
-			}//while close
+					if(!a.equals(b));{	//	a와 b의 값을 비교
+					System.out.println(file1 + ":=> " +cnt+"line :"+a);	//info 2파일 출력
+					System.out.println(file2 + ":=> " +cnt+"line :"+b);	// info1 파일 출력
+					System.out.println();
+					}
+					}else break;
+			}//while문 끝나는 지점
 		}catch(Exception e) {
 		}
 	}
