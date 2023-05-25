3ca49bb2b0a555bc306d4380b17755589f60b698
Malvin222 <127707299+Malvin222@users.noreply.github.com>
2023-05-25 17:25:04 +0900

    Delete Oracle/study0525 directory

--- Oracle/study0525/nvl.sql
+++
@@ -1,6 +0,0 @@
---NVL()함수) nvl(필드값,세팅값) ->필드값이 null인경우 세팅값 리턴
-select ename,nvl(mgr,0) mgr from emp;
-
-select ename,sal,comm,(sal+nvl(comm,0)) from emp;
-                            --nvl2(컬럼,null이 아닌경우,null인경우)
-select ename,sal,comm,(sal+nvl2(comm,comm+100,0)) from emp;
